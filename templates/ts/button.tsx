7f6580ecdbd9c0d5ca66ddfb8240c54736c13b78
pankajkoree <pankajkori2057@gmail.com>
2025-11-23 20:37:23 +0545

    variants in buttons

--- templates/ts/button.tsx
+++ templates/ts/button.tsx
@@ -5,6 +5,9 @@ const styles = cva("px-4 py-2 rounded", {
   variant: {
     primary: "bg-blue-600 text-white",
     secondary: "bg-gray-600 text-white",
+    signup: "bg-green-400 text-white hover:bg-green-600",
+    login: "bg-blue-400 text-white hover:bg-blue-600",
+    bordered: "border border-gray-600 bg-black text-white hover:bg-gray-500",
   },
 });
 
