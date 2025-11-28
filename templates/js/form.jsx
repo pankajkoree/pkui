8d535608cae2c99d3325b1f235364c6862e58f60
pankajkoree <pankajkori2057@gmail.com>
2025-11-28 13:19:15 +0545

    form

---
+++ templates/js/form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Label from "./label";
+
+const Form = () => {
+  return (
+    <div>
+      <Label>{labelName}</Label>
+    </div>
+  );
+};
+
+export default Form;
